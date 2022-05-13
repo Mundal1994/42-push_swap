b66e790ab74094466757f49395a69a0369c132db
molesen <molesen@student.hive.fi>
2022-05-13 18:20:56 +0300

    worked on different version of algorythm testing

--- swap/sort_stack.c
+++ swap/sort_stack.c
@@ -188,16 +188,35 @@ while loop
 
 	ex if putting top element in the bottom
 		if (stack->a[top_a] < stack->a[top_a + 1])
+			if (stack->b_empty == FALSE && stack->a[stack->top_a] < stack->b[stack->top_b])
+				//maybe check how to quickliest get it to the right position
+				if (stack->a[top_a] > stack->b[top_b])
+					if (stack->b[stack->top_b + 1] > stack->b[stack->bottom - 1])
+						sb swap top of stack b
+					//make some kind of looping either back and forward and have it loop back after pushing it to correct position
+					//rr or rb thingy
+					push to b
+					// when moving it back we need to check if moving same direction would make sense for stack a
+					//ex if stack->a[top_a] > stack->b[top_b + 1] && stack->a[top_a + 1] < stack->b[top_b + 1]
+					// if this happens the next move HAS TO BE 'pa'
+					// only can do this if bottom is lowest number and i have to make sure it is assembled in reverse order so i can push it to top of stack b
+
 			if (stack->a[stack->top_a + 1] > stack->a[stack->bottom - 1])
 				switch(maybe also here with b_stack if possible)
 		if (stack->a[top_a] > stack->a[stack->bottom - 1])
-			reverse to bottom
+			if (stack->b_empty == FALSE && stack->b[top_b] < stack->b[stack->bottom - 1])
+				reverse to bottom rr
+			else
+				reverse to bottom ra
 		else
 			push to b
 				//make sure b is in order and if moves make sense to do on both stacks it will be done here
 		to know weather to put current number to the bottom you check if the top number is bigger than the bottom number
 		
-	if puttting bottom to top
+		when biggest number is at bottom
+		if (stack->a[top_a] < stack->a[top_a + 1])
+
+	if putting bottom to top
 		if (nbr < nbr + 1)
 			switch
 		else
@@ -207,6 +226,10 @@ while loop
 	depending on which is longer we will do either rra or ra we rotate until the bottom ordered stack is on top and we will move stuff from stack b
 	if the other sorted stack isn't the end we will move when stack->b top is smaller than the stack-a[bottom];
 
+
+when two sorted stacks in stack a and one sorted stack b i will merge them together
+
+check at what point in the array of stack->a is sorted correctly
 
 ALWAYS keep stack b in order as much as possible
 
@@ -234,6 +257,272 @@ ALWAYS keep stack b in order as much as possible
 
 
 
+
+
+
+./checker 0 4 7 -9 9 2 10 -4 5 3 1 8 -6 6
+pb
+pb
+ra
+pb
+rr
+pb
+sb
+ra
+rrb
+pb
+rb
+rr
+pb
+sb
+rra
+pb
+rrb
+rrb
+rrb
+pb
+sa
+rb
+rb
+rb
+pb
+rb
+rr
+pb
+pb
+rra
+pa
+pa
+pa
+pa
+pa
+pa
+pa
+pa
+pa
+pa
+pa
+
+41
+
+
+
+
+moving it downwards instead of up rra
+pb
+pb
+rra
+rra
+pb
+rb
+pb
+pb
+pb
+rr
+sa
+rr
+rb
+rb
+pb
+rb
+rb
+pb
+sa
+rrb
+rrb
+pb
+sa
+pb
+rrb
+pb
+rrb
+rrb
+pa
+pa
+pa
+pa
+sb
+pa
+sb
+pa
+pa
+pa
+pa
+pa
+pa
+
+
+41
+
+
+top two and bottom system
+pb
+rra
+sa
+rra
+pb
+pb
+rrb
+sb
+pb
+sa
+pb
+rra
+sa
+rra
+rb
+pa
+rra
+pb
+rra
+rra
+ss
+pb
+sb
+pa
+pa
+rra
+pb
+rra
+rra
+ss
+ra
+pa
+sb
+pa
+pa
+pa
+pa
+
+37
+
+keep track of top and bottom in each stack so i then can know if they have been divided with all the numbers
+
+
+./checker 5 4 3 2 1
+ra
+sa
+pb
+sa
+pb
+ss
+ra
+pa
+pa
+rra
+
+10
+
+keep middle 3 everything else goes to stack b
+
+pb
+pb
+ra
+ss
+pa
+pa
+ra
+ra
+
+8
+
+jefs version
+
+sa
+ra
+pb
+ra
+pb
+ra
+pa
+pa
+
+8
+
+./checker 9 8 7 6 5 4 3
+sa
+rra
+pb
+rra
+pb
+rra
+pb
+rra
+pb
+rra
+pa
+pa
+pa
+pa
+
+14
+
+ra
+sa
+pb
+sa
+pb
+ss
+pb
+sa
+rb
+pb
+rb
+sa
+pb
+rb
+pa
+pa
+pa
+pa
+pa
+
+19
+
+
+
+sort only top two and bottom
+sa
+rra
+rra
+sa
+pb
+rra
+sa
+pb
+rra
+sa
+pb
+rra
+sa
+pa
+pa
+pa
+
+16
+
+
+./checker 8 4 9 3 6 5 7
+sa
+rra
+sa
+rra
+sa
+pa
+rra
+sa
+rra
+pa
+sa
+
+11
+
+
+
+
+keep track of lowest and heighest number in each stack
+stack->b_low
+stack->b_heigh
 
 
 */
